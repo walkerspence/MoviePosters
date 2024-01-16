58df157018c0d6a0809a2820cb8672b128c60d74
Walker Spence <walker.spence@gmail.com>
2024-01-15 18:26:34 -0800

    misc fixes, was this broken the whole time? lol

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 if __name__ == "__main__":
     file_name = input("Place the file you wish to convert in the 'input' folder and type its name: ")
     poster_name = input("Name your poster png: ")
-    num_stills = int(input("Select the number of layers you want your image to have"))
+    num_stills = int(input("Enter the number of layers you want your image to have: "))
     vp = processing.VideoProcessor(file_name)
     frames = vp.get_n_stills(num_stills)
     ip = processing.ImgProcessor(frames)
